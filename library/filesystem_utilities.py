4c2cda349596ba418d80dfc1e85d8444d3768772
Stylianos Gregoriou <s.gregoriou@cyi.ac.cy>
2024-11-18 18:07:51 +0200

    Improved log formatter by restring output to 80 characters per line

--- library/filesystem_utilities.py
+++ library/filesystem_utilities.py
@@ -1,6 +1,7 @@
 import os
 
 import logging
+import textwrap
 
 
 def is_valid_directory(directory_path):
@@ -22,6 +23,13 @@ def create_directory(directory_path):
         os.makedirs(directory_path)
 
 
+class WrappedFormatter(logging.Formatter):
+    def format(self, record):
+        # Use textwrap to wrap the log message to 80 characters
+        wrapped_message = textwrap.fill(super().format(record), width=80)
+        return wrapped_message
+
+
 def setup_logging(log_directory, log_filename):
     """Setup logging configuration."""
 
@@ -31,8 +39,13 @@ def setup_logging(log_directory, log_filename):
         # Set the logging level to DEBUG
         level=logging.DEBUG,  # Set the log level (DEBUG, INFO, WARNING, etc.)
         # Set the log message format
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", 
-        handlers=[
-            logging.FileHandler(log_path, mode='w') # Overwrite the log file
-        ]
+        format="%(asctime)s - %(levelname)s - %(message)s", 
+        # Overwrite an existing log file
+        handlers=[logging.FileHandler(log_path, mode='w')]
     )
+
+    # Get the default handler
+    file_handler = logging.getLogger().handlers[0]
+
+    # Set the custom WrappedFormatter
+    file_handler.setFormatter(WrappedFormatter("%(asctime)s - %(levelname)s - %(message)s"))
