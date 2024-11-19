f0a037e32a51c93d6b18c50ce72e99c070d2cda2
Stylianos Gregoriou <s.gregoriou@cyi.ac.cy>
2024-11-19 07:34:00 +0200

    Established a format for future mass processing of log files

--- bash_scripts/analyze_log_files.sh
+++ bash_scripts/analyze_log_files.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 WORKING_DIRECTORY=".."
+SOURCE_SCRIPTS_DIRECTORY="${WORKING_DIRECTORY}/src"
+RAW_FILES_DIRECTORY="${WORKING_DIRECTORY}/data_files/raw"
+PROCESSED_FILES_DIRECTORY="${WORKING_DIRECTORY}/data_files/processed"
 
-python $WORKING_DIRECTORY"/src/log_files_analysis.py" \
-    -log_dir $WORKING_DIRECTORY"/data_files/raw/sign_squared_violation/KL_several_vectors_varying_configs_and_n" \
-    -out_dir $WORKING_DIRECTORY"/data_files/processed"
+WORKING_PROJECT="sign_squared_violation/KL_several_vectors_varying_configs_and_n"
+python "${SOURCE_SCRIPTS_DIRECTORY}/log_files_analysis.py" \
+    -qpb_log_dir "${RAW_FILES_DIRECTORY}/${WORKING_PROJECT}" \
+    -out_dir "${PROCESSED_FILES_DIRECTORY}/${WORKING_PROJECT}"
