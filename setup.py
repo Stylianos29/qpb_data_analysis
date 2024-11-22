0b5d470cd404b0f0e015239b6babf02ac1d12e89
Stylianos Gregoriou <s.gregoriou@cyi.ac.cy>
2024-11-22 15:05:34 +0200

    Updated the whole script

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 from setuptools import setup, find_packages # type: ignore
 
 setup(
-    name="qpb_data_analysis",      # Package name
-    version="0.1.0",               # Version
-    description="Tools for processing and analyzing data files.",  # Description
-    author="Stylianos Gregoriou",  # Author
-    package_dir={"": "src"},       # Map the 'src/' directory as the package root
-    packages=find_packages(where="src"),  # Find packages in 'src/'
-    install_requires=[
+    name="qpb_data_analysis",
+    version="0.1",
+    description="A Python project for data analysis and processing of qpb data.",
+    author="Stylianos Gregoriou",
+    author_email="s.gregoriou@cyi.ac.cy",
+    url="https://github.com/Stylianos29/qpb_data_analysis",
+    # Tell setuptools to look in the 'core' directory for packages
+    packages=find_packages(where="core"), 
+    package_dir={"": "core"},  # The root of the package is now 'core'
+    install_requires=[  # List your dependencies here
         "click",
         "gvar",
         "lsqfit",
         "h5py",
-    ],  # Dependencies
-    python_requires=">=3.8",       # Minimum Python version
+    ]
 )
