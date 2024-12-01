62065bc837203c95059523d71cbd6c05a25716d9
Stylianos Gregoriou <s.gregoriou@cyi.ac.cy>
2024-12-01 12:35:18 +0200

    Resolved the issue of not preventing copying the depth-2 subdirectories

--- bash_scripts/utils/back_up_data_files.sh
+++ bash_scripts/utils/back_up_data_files.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-TODO: It still copies the depth-2 subdirectories. The issue must be resolved.
-
 SOURCE_DIR="/nvme/h/cy22sg1/Data_analysis/data_files"
 DEST_DIR="/onyx/qdata/cy22sg1/data_files"
 LOG_FILE="/nvme/h/cy22sg1/Data_analysis/rsync.log"
@@ -14,14 +12,15 @@ find "$SOURCE_DIR" -mindepth 2 -maxdepth 2 -type d | while read subdirDepth2; do
     
     # Ensure the subdirectory is correctly compressed into a tar.gz file
     # inside the parent directory
-    tar -czf "$parent_dir/$(basename "$subdirDepth2").tar.gz" -C "$parent_dir" "$(basename "$subdirDepth2")"
+    tar -czf "$parent_dir/$(basename "$subdirDepth2").tar.gz" -C "$parent_dir" \
+                                                "$(basename "$subdirDepth2")"
 done
 
 # Step 2: Rsync to backup files (only .tar.gz and markdown files, maintaining
-# depth-1 structure)
+# depth-1 structure, and excluding depth-2 directories entirely)
 rsync -av --partial --delete --log-file="$LOG_FILE" \
-    --include="*/" --include="*.tar.gz" --include="*.md" --exclude="*/" \
-    --exclude="*/*/" \
+    --include="/*/" --include="*.tar.gz" --include="*.md" \
+    --exclude="/*/*/" --exclude="*" \
     "$SOURCE_DIR/" "$DEST_DIR/"
 
 # Step 3: Clean up by deleting the .tar.gz files from the source directory
