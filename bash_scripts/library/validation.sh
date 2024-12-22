bb0ad45ace3bfab336fbe2d74a73fee14c66a727
Stylianos Gregoriou <s.gregoriou@cyi.ac.cy>
2024-12-22 09:47:47 +0200

    Corrected typos

--- bash_scripts/library/validation.sh
+++ bash_scripts/library/validation.sh
@@ -2,29 +2,31 @@
 
 # MULTIPLE SOURCING GUARD
 
-# Prevent multiple sourcing of this script by exiting if UTILITIES_SH_INCLUDED
-# is already set. Otherwise, set UTILITIES_SH_INCLUDED to mark it as sourced.
-[[ -n "${UTILITIES_SH_INCLUDED}" ]] && return
-UTILITIES_SH_INCLUDED=1
+# Prevent multiple sourcing of this script by exiting if VALIDATION_SH_INCLUDED
+# is already set. Otherwise, set VALIDATION_SH_INCLUDED to mark it as sourced.
+[[ -n "${VALIDATION_SH_INCLUDED}" ]] && return
+VALIDATION_SH_INCLUDED=1
 
 # CUSTOM FUNCTIONS DEFINITIONS
 
 check_directory_for_changes() {
     # Function to check if a directory has changed since the last validation.
     # Arguments:
-    #   $1: DATA_SET_DIR - The directory to check.
-    #   $2: TIMESTAMP_DIR - The directory where the timestamp files are stored.
+    #   $1: DATA_SET_DIRECTORY - Directory to check.
+    #   $2: TIMESTAMP_DIRECTORY - Directory where timestamp files are stored.
 
-    local DATA_SET_DIR="$1"
-    local TIMESTAMP_DIR="$2"
-    local TIMESTAMP_FILE="$TIMESTAMP_DIR/$(basename "$DATA_SET_DIR").timestamp"
+    local DATA_SET_DIRECTORY="$1"
+    local TIMESTAMP_DIRECTORY="$2"
+    
+    local TIMESTAMP_FILE="$TIMESTAMP_DIRECTORY/"
+    TIMESTAMP_FILE+="$(basename "$DATA_SET_DIRECTORY").timestamp"
 
     # Ensure the timestamp directory exists
-    mkdir -p "$TIMESTAMP_DIR"
+    mkdir -p "$TIMESTAMP_DIRECTORY"
 
     # Get the current modification time of the data files directory
     local CURRENT_TIMESTAMP
-    CURRENT_TIMESTAMP=$(stat -c %Y "$DATA_SET_DIR")
+    CURRENT_TIMESTAMP=$(stat -c %Y "$DATA_SET_DIRECTORY")
 
     # Check if a stored timestamp file exists
     if [[ -f "$TIMESTAMP_FILE" ]]; then
