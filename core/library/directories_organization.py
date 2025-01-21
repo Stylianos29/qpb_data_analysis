6c2a70a6491c7b9b601dfe1a87e7d51537887ab6
Stylianos Gregoriou <s.gregoriou@cyi.ac.cy>
2025-01-21 13:38:50 +0200

    Reformatted its contents

--- core/library/directories_organization.py
+++ core/library/directories_organization.py
@@ -2,18 +2,18 @@ import os
 
 
 def extract_relative_path(file_full_path, root_directory):
-    '''Returns the relative path from the given full path of a file or directory with respect to the given root directory. NOTE: A leading "/" needs to be removed as well from the resulting relative path.'''
+    """Returns the relative path from the given full path of a file or directory with respect to the given root directory. NOTE: A leading "/" needs to be removed as well from the resulting relative path."""
 
     return file_full_path.replace(root_directory, "")
 
 
 def replicate_directory_structure(source_directory, target_directory):
-    
+
     # Create the target directory if it doesn't exist
     if not os.path.exists(target_directory):
         print(f"Warning: Target directory '{target_directory}' does not exist.")
         print("Exiting.")
-        
+
         return
 
     new_subdirectories_lists = list()
@@ -31,14 +31,17 @@ def replicate_directory_structure(source_directory, target_directory):
             os.makedirs(new_subdirectory)
 
         # Limit the depth to 2 by removing subdirectories beyond depth 2
-        if new_subdirectory.count(os.path.sep) - target_directory.count(os.path.sep) >= 2:
+        if (
+            new_subdirectory.count(os.path.sep) - target_directory.count(os.path.sep)
+            >= 2
+        ):
             del dirs[:]
-    
+
     return new_subdirectories_lists
 
 
 def list_leaf_subdirectories(directory):
-    '''Return a list of all the subdirectories excluding those with other further subdirectories and files.'''
+    """Return a list of all the subdirectories excluding those with other further subdirectories and files."""
 
     leaf_subdirectories = []
     for root, dirs, files in os.walk(directory):
@@ -54,9 +57,9 @@ def change_root(old_path, old_root, new_root):
         raise ValueError("old_path does not start with old_root")
 
     # Remove the old_root from the beginning of old_path
-    relative_path = old_path[len(old_root)+1:]
+    relative_path = old_path[len(old_root) + 1 :]
 
     # Create the new path by joining new_root and the relative path
     new_path = os.path.join(new_root, relative_path)
-    
+
     return new_path
