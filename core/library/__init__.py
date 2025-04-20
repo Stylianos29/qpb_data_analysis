5456b167ae3c8a882c1d79e8ab1d05269d6f039a
Stylianos Gregoriou <s.gregoriou@cyi.ac.cy>
2025-04-20 20:25:09 +0300

    Made "validate_input_directory" custom function part of library's public API

--- core/library/__init__.py
+++ core/library/__init__.py
@@ -1,3 +1,4 @@
 from .constants import ROOT, RAW_DATA_FILES_DIRECTORY, PROCESSED_DATA_FILES_DIRECTORY
 from .data_processing import DataFrameAnalyzer
 from .data_files_checks import get_yes_or_no_user_response
+from .filesystem_utilities import validate_input_directory
