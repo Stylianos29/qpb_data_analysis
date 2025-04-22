2c6fe20183a852102ad603fbce371e0bf8f42af1
Stylianos Gregoriou <s.gregoriou@cyi.ac.cy>
2025-04-22 17:41:57 +0300

    Added "load_csv()" custom function to custom library's public API

--- core/library/__init__.py
+++ core/library/__init__.py
@@ -6,7 +6,7 @@ and data validation.
 """
 
 from .constants import ROOT, RAW_DATA_FILES_DIRECTORY, PROCESSED_DATA_FILES_DIRECTORY
-from .data_processing import DataFrameAnalyzer
+from .data_processing import load_csv, DataFrameAnalyzer
 from .data_files_checks import get_yes_or_no_user_response
 from .filesystem_utilities import (
     validate_input_directory,
@@ -17,6 +17,7 @@ __all__ = [
     'ROOT',
     'RAW_DATA_FILES_DIRECTORY',
     'PROCESSED_DATA_FILES_DIRECTORY',
+    'load_csv',
     'DataFrameAnalyzer',
     'get_yes_or_no_user_response',
     'validate_input_directory',
