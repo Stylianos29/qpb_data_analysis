2ae57dd2b972ee4f86699ff5e39f4a3a374fc6ee
Stylianos Gregoriou <s.gregoriou@cyi.ac.cy>
2025-04-24 09:26:03 +0300

    Add "TableGenerator" class to library's public API

--- core/library/__init__.py
+++ core/library/__init__.py
@@ -6,7 +6,7 @@ and data validation.
 """
 
 from .constants import ROOT, RAW_DATA_FILES_DIRECTORY, PROCESSED_DATA_FILES_DIRECTORY
-from .data_processing import load_csv, DataFrameAnalyzer
+from .data_processing import load_csv, DataFrameAnalyzer, TableGenerator
 from .data_files_checks import get_yes_or_no_user_response
 from .filesystem_utilities import (
     validate_input_directory,
@@ -19,6 +19,7 @@ __all__ = [
     'PROCESSED_DATA_FILES_DIRECTORY',
     'load_csv',
     'DataFrameAnalyzer',
+    'TableGenerator',
     'get_yes_or_no_user_response',
     'validate_input_directory',
     'validate_input_script_log_filename',
