c0d1b66897c0a2fa1f264b76e7964d482eac466c
Stylianos Gregoriou <s.gregoriou@cyi.ac.cy>
2025-04-28 18:21:00 +0300

    Included again partial pion effective mass functionalities

--- bash_scripts/workflows/analyze_invert_processed_data_files_set.sh
+++ bash_scripts/workflows/analyze_invert_processed_data_files_set.sh
@@ -266,17 +266,17 @@ python_script_path="${PYTHON_SCRIPTS_DIRECTORY}/post_processing_analysis"
 python_script_path+="/calculate_effective_mass_estimates.py"
 check_if_file_exists "$python_script_path" || exit 1
 
-# python $python_script_path \
-#     --input_correlators_jackknife_analysis_hdf5_file_path \
-#                             "$correlators_jackknife_analysis_hdf5_file_path" \
-#     --plots_directory "$data_files_set_plots_directory" \
-#     --plot_g5g5_correlators --plot_effective_mass_correlators \
-#     --output_pion_effective_mass_estimates_csv_filename \
-#                                 "$PION_EFFECTIVE_MASS_ESTIMATE_CSV_FILENAME" \
-#     --enable_logging \
-#     --log_file_directory "$auxiliary_files_directory" \
-#     || failed_python_script $python_script_path
-#     # --zoom_in_effective_mass_correlators_plots \
+python $python_script_path \
+    --input_correlators_jackknife_analysis_hdf5_file_path \
+                            "$correlators_jackknife_analysis_hdf5_file_path" \
+    --plots_directory "$data_files_set_plots_directory" \
+    --plot_g5g5_correlators --plot_effective_mass_correlators \
+    --output_pion_effective_mass_estimates_csv_filename \
+                                "$PION_EFFECTIVE_MASS_ESTIMATE_CSV_FILENAME" \
+    --enable_logging \
+    --log_file_directory "$auxiliary_files_directory" \
+    || failed_python_script $python_script_path
+    # --zoom_in_effective_mass_correlators_plots \
 
 # Log successful calculation
 log_message="Pion effective mass estimate analysis for "
