9729989fe4299de39ac68ff1128f233754432520
Stylianos Gregoriou <s.gregoriou@cyi.ac.cy>
2025-05-17 20:49:31 +0300

    Added EnhancedHDF5Analyzer to data module exports

--- core/library/data/__init__.py
+++ core/library/data/__init__.py
@@ -1,4 +1,5 @@
 from .analyzer import DataFrameAnalyzer
+from .hdf5_analyzer import EnhancedHDF5Analyzer
 from .table_generator import TableGenerator
 
-__all__ = ["DataFrameAnalyzer", "TableGenerator"]
+__all__ = ["DataFrameAnalyzer", "TableGenerator", "EnhancedHDF5Analyzer"]
