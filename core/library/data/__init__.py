48925dece170eb4de200f26eddd41fd28e9dcc02
Stylianos Gregoriou <s.gregoriou@cyi.ac.cy>
2025-05-18 10:11:48 +0300

    Included load_csv() custom function in the data module initialization

--- core/library/data/__init__.py
+++ core/library/data/__init__.py
@@ -1,5 +1,6 @@
 from .analyzer import DataFrameAnalyzer
 from .hdf5_analyzer import EnhancedHDF5Analyzer
 from .table_generator import TableGenerator
+from .processing import load_csv
 
-__all__ = ["DataFrameAnalyzer", "TableGenerator", "EnhancedHDF5Analyzer"]
+__all__ = ["DataFrameAnalyzer", "TableGenerator", "EnhancedHDF5Analyzer", "load_csv"]
