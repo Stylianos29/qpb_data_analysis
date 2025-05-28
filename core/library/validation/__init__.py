6a1c16bb934eb10c1019fceaa7e21505472de75d
Stylianos Gregoriou <s.gregoriou@cyi.ac.cy>
2025-05-28 20:58:23 +0300

    feat: Add validation package with Click validators
    Create validation package with initial __init__.py that exports
    the core Click validator functions:
    - validate_input_directory
    - validate_output_directory
    - validate_input_file
    - validate_output_file
    
    This __init__.py establishes the public API for the validation
    package, making the Click validators easily importable by other
    modules in the project.

---
+++ core/library/validation/__init__.py
@@ -0,0 +1,14 @@
+from .click_validators import (
+    validate_input_directory,
+    validate_output_directory,
+    validate_input_file,
+    validate_output_file,
+)
+
+__all__ = [
+    # Click Validators
+    "validate_input_directory",
+    "validate_output_directory",
+    "validate_input_file",
+    "validate_output_file",
+]
