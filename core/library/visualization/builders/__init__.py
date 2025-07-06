8a9a96b974b34d593ba72607e91bf26af927c99f
Stylianos Gregoriou <s.gregoriou@cyi.ac.cy>
2025-07-06 16:37:03 +0300

    Add __init__.py to define public API for builders with PlotTitleBuilder and

---
+++ core/library/visualization/builders/__init__.py
@@ -0,0 +1,8 @@
+from .filename_builder import PlotFilenameBuilder
+from .title_builder import PlotTitleBuilder
+
+# Define public API
+__all__ = [
+    "PlotTitleBuilder",
+    "PlotFilenameBuilder",
+]
