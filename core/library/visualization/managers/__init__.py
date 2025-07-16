035819a387da93ac7436c170e8322a9240e642de
Stylianos Gregoriou <s.gregoriou@cyi.ac.cy>
2025-07-16 13:45:58 +0300

    chore: Add managers package __init__ file
    - Import core plot manager classes into managers namespace
    - Define __all__ to explicitly specify public API
    - Include PlotAnnotationManager, PlotDataProcessor, PlotFileManager,
      PlotLayoutManager, and PlotStyleManager in public interface

--- core/library/visualization/managers/__init__.py
+++ core/library/visualization/managers/__init__.py
@@ -1,9 +1,14 @@
+from .annotation_manager import PlotAnnotationManager
+from .data_processor import PlotDataProcessor
 from .file_manager import PlotFileManager
 from .layout_manager import PlotLayoutManager
 from .style_manager import PlotStyleManager
 
+
 # Define public API
 __all__ = [
+    "PlotAnnotationManager",
+    "PlotDataProcessor",
     "PlotFileManager",
     "PlotLayoutManager",
     "PlotStyleManager",
